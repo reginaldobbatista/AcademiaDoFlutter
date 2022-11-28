8a5d3c05deaee23e485245c938a263c44ab1cac5
unknown <PMRR@SACCM-03.seed.rr.gov.br>
2022-11-28 19:07:01 -0400

    First commit

---
+++ bin/academia_do_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:academia_do_flutter/academia_do_flutter.dart' as academia_do_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${academia_do_flutter.calculate()}!');
+}
---
+++ test/academia_do_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:academia_do_flutter/academia_do_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
