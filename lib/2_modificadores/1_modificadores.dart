7227252b303102b704ed3bea7f7e0c104c3bd863
reginaldoBBatista <PMRR@SACCM-03.seed.rr.gov.br>
2023-05-07 12:50:46 -0400

    2.1_modificadores

---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,30 @@
+
+
+void main(){
+
+  var nomeCompleto = 'Reginaldo Barbosa';
+  print(nomeCompleto);
+
+
+  // Variáveis finais:
+  // Não podem ser alteradas depois de inicializadas (Imutáveis)
+  // São definidas do programa em tempo de execuçaõ (Runtime)
+  // utilize sem moderação
+  final nomeCompletoFinal = 'Reginaldo';
+  final nomeCompletoFinal2 = nomeCompleto;
+
+
+  // Variáveis Const
+  // Não podem ser alteradas depois de inicializadas (Imutáveis)
+  // São definidas no programa em tempo de COMPILAÇÃO!
+  // Não podem receber valores de outras variáveis a não ser que essas variáveis
+  // forem const também
+  // Utilize sem moderação
+  const nomeCompletoConst = 'Reginaldo';
+  const nomeCompletoConst2 = nomeCompletoConst;
+
+ 
+
+
+
+}
